37f5463e5aa868af92b1347c884d0456a27c8bf0
Reanz_Arthur <reqbahrf@gmail.com>
2024-08-22 21:35:38 +0800

    fix: :adhesive_bandage: minor fix on the staff account name render.

--- resources/views/staffView/staffDashboard.blade.php
+++ resources/views/staffView/staffDashboard.blade.php
@@ -547,7 +547,7 @@
                                     width="32" height="32"
                                     class="object-fit-cover rounded-circle border border-1 border-black"
                                     alt="">
-                                <p class="m-0 fw-bold">{{ session('name') }}</p>
+                                <p class="m-0 fw-bold">{{ Auth::user()->orgusername->full_name }}</p>
                             </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated py-0"
