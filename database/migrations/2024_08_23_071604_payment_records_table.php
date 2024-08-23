6fd903049dd8fece1d895e5da2433b869f3c2b16
Reanz_Arthur <reqbahrf@gmail.com>
2024-08-23 15:43:34 +0800

    refactor: :card_file_box: add migration for the payment history and file links records

---
+++ database/migrations/2024_08_23_071604_payment_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_records', function (Blueprint $table) {
+            $table->char('Project_id', 15)->primary()->unique()->collation('utf8mb4_bin');
+            $table->string('transaction_id', 64)->unique();
+            $table->decimal('amount', 10, 2);
+            $table->string('payment_status', 64);
+            $table->string('payment_method', 64);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->foreign('Project_id')->references('Project_id')->on('project_info')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_records');
+    }
+};
---
+++ database/migrations/2024_08_23_072537_project_file_links.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_file_links', function (Blueprint $table) {
+           $table->char('Project_id', 15)->primary()->unique()->collation('utf8mb4_bin');
+           $table->string('file_name', 64);
+           $table->string('file_link', 255);
+           $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+           $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+           $table->foreign('Project_id')->references('Project_id')->on('project_info')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_file_links');
+    }
+};
