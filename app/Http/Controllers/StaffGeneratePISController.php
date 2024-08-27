0ef80371fe6ae0fa8d3bf49193e0f482a8bebd88
Reanz_Arthur <reqbahrf@gmail.com>
2024-08-27 12:59:13 +0800

    feat: :construction: Project Information Sheet pdf layout.

--- app/Http/Controllers/StaffGeneratePISController.php
+++ app/Http/Controllers/StaffGeneratePISController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\Browsershot\Browsershot;
+use Illuminate\Support\Facades\View;
+use Mpdf\Mpdf;
 
 class StaffGeneratePISController extends Controller
 {
 
     public function index(Request $request)
     {
-       $samplePDF = view('staffView.outputs.InformationSheetTable')->render();
+        $html = view('staffView.outputs.InformationSheetTable')->render();
+        $mpdf = new Mpdf([
+            'mode' => 'utf-8',
+            'format' => 'A4',
+            'margin_top' => 2,
+            'margin_bottom' => 2,
+            'margin_left' => 2,
+            'margin_right' => 2,
+            'tempDir' => storage_path('app/mpdf'), // Use a custom temporary directory
+        ]);
 
-       Browsershot::html($samplePDF)
-            ->format('A4')
-            ->save(public_path('pis.pdf'));
+        $mpdf->WriteHTML($html);
+        $mpdf->Output('hello.pdf', 'D'); // Download the PDF with the filename hello.pdf
 
-            return response()->download(public_path('pis.pdf'));
     }
+
 }
