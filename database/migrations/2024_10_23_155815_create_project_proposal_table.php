2b6e6b5a6a3591b7aea21cc216569613d4aa8481
Reanz_Arthur <reqbahrf@gmail.com>
2024-10-23 18:08:01 +0800

    refactor: Add ProjectProposal model with relationships and fillable attributes, change also the foreign key.

--- database/migrations/2024_10_23_155815_create_project_proposal_table.php
+++ database/migrations/2024_10_23_155815_create_project_proposal_table.php
@@ -14,12 +14,12 @@ return new class extends Migration
         Schema::create('project_proposal', function (Blueprint $table) {
             $table->id();
             $table->char('Project_id', 15)->nullable()->collation('utf8mb4_bin');
-            $table->integer('business_id')->unsigned()->nullable();
+            $table->integer('application_id')->unsigned()->nullable();
             $table->json('data');
             $table->enum('submission_status', ['Draft', 'Submitted']);
             $table->timestamps();
 
-            $table->foreign('business_id')->references('id')->on('business_info')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('application_id')->references('id')->on('application_info')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('Project_id')->references('Project_id')->on('project_info')->onDelete('cascade')->onUpdate('cascade');
         });
     }
