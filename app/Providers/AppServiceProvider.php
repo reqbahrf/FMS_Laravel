f8cf9cc0fc7c0e940eb20ed9f3dbf8ab7fbf85ba
Reanz_Arthur <reqbahrf@gmail.com>
2024-11-30 18:44:26 +0800

    refactor: adjusting the telescopeServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,10 @@ namespace App\Providers;
 
 use App\Models\PaymentRecord;
 use App\Models\ApplicationInfo;
-use Laravel\Telescope\Telescope;
 use Illuminate\Support\ServiceProvider;
 use App\Observers\PaymentRecordObserver;
 use App\Observers\ApplicationInfoObserver;
-use Workbench\App\Providers\TelescopeServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,10 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\Telescope::class)) {
+
+        if ($this->app->environment('local') && class_exists('Laravel\Telescope\TelescopeServiceProvider')) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-            \Laravel\Telescope\Telescope::night();
         }
     }
 
