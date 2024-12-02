a47514ce33b186e9978664d42ca9b467abc7f4a2
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-03 00:30:35 +0800

    refactor: update websocket port

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,8 +7,8 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: 8080,
-    wssPort: 8080,
-    forceTLS: false,
+    wsPort: 8090,
+    wssPort: 8090,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
