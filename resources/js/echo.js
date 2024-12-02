29770224bc0e33a6e531eea876020cbf9a55e232
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-03 00:42:20 +0800

    refator:some adjustment on the echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,18 @@
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
+    broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: 8090,
-    wssPort: 8090,
+    wsHost: import.meta.env.VITE_REVERB_HOST || "srv656676.hstgr.cloud",
+    wsPort: import.meta.env.VITE_REVERB_PORT || 8090,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 8090,
     forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ["ws", "wss"],
+    disableStats: true,
+    encrypted: true,
+    cluster: "mt1",
+    authEndpoint: "/broadcasting/auth",
 });
