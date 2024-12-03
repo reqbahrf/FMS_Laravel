897c4329ce3b5c71a391509cad4f2e85d2c5710c
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-03 09:59:36 +0800

    refactor: use env import for port

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,9 +6,9 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST || "srv656676.hstgr.cloud",
-    wsPort: null,
-    wssPort: 443,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: true,
     enabledTransports: ["ws", "wss"],
     encrypted: true,
