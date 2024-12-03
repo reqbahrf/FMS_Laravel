f5bd583b26b6646c66b558806bc2ccb055bfe272
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-03 08:32:22 +0800

    refactor: another port change

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,11 +7,9 @@ window.Echo = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST || "srv656676.hstgr.cloud",
-    wsPort: import.meta.env.VITE_REVERB_PORT || 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 8080,
+    wsPort: 8080,
+    wssPort: 8080,
     forceTLS: true,
     enabledTransports: ["ws", "wss"],
-    disableStats: true,
     encrypted: true,
-    cluster: "mt1",
 });
