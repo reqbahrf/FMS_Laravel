42466e0aa0c83095db711de5dcf2d063d5310cc7
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-03 09:41:14 +0800

    refactor: specify the port to 443.

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -8,7 +8,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST || "srv656676.hstgr.cloud",
     wsPort: null,
-    wssPort: import.meta.env.VITE_REVERB_PORT || 443,
+    wssPort: 443,
     forceTLS: true,
     enabledTransports: ["ws", "wss"],
     encrypted: true,
