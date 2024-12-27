db04d183e1e1d3e8adf5886b62a3b45385542013
Reanz_Arthur <reqbahrf@gmail.com>
2024-12-27 15:21:20 +0800

    refactor: improved formatting and add the Project nav tab for the mobile device responsive design.

--- resources/views/CooperatorView/CooperatorApprovedPage.blade.php
+++ resources/views/CooperatorView/CooperatorApprovedPage.blade.php
@@ -6,7 +6,10 @@
         <ul class="navbar-nav">
             <li class="nav-item mb-2 minimize">
                 <div class="navlogo d-flex justify-content-center align-items-center">
-                    <img src="{{ asset('DOST_ICON.svg') }}" alt="DOST logo">
+                    <img
+                        src="{{ asset('DOST_ICON.svg') }}"
+                        alt="DOST logo"
+                    >
                     <div class="logoTitleLScreen row position-relative h-100 w-75">
                         <div class="position-absolute top-50">
                             <p class="sideTextMain text-white m-0 w-100"></p>
@@ -18,52 +21,78 @@
                 </div>
             </li>
             <li class="nav-item main-Nav mb-2">
-                <a href="#" id="InformationTab"
+                <a
+                    class="mb-2 d-flex align-items-center"
+                    id="InformationTab"
+                    href="#"
                     onclick="loadPage('{{ route('Cooperator.dashboard') }}','InformationTab');"
-                    class="mb-2 d-flex align-items-center">
+                >
                     <i class="ri-dashboard-3-fill ri-2x"></i>
                     <span class="nav-text ml-2">Dashboard</span>
                 </a>
             </li>
             <li class="nav-item main-Nav mb-2">
-                <a href="#" id="requirementTab"
+                <a
+                    class="mb-2 d-flex align-items-center"
+                    id="requirementTab"
+                    href="#"
                     onclick="loadPage('{{ route('Cooperator.Requirements') }}','requirementTab');"
-                    class="mb-2 d-flex align-items-center">
+                >
                     <i class="ri-file-list-2-fill ri-2x"></i>
                     <span class="nav-text ml-2">Requirements</span>
                 </a>
             </li>
             <li class="nav-item main-Nav mb-2">
-                <a href="#" id="ProjectsTab"
+                <a
+                    class="mb-2 d-flex align-items-center"
+                    id="ProjectsTab"
+                    href="#"
                     onclick="loadPage('{{ route('Cooperator.myProjects') }}','ProjectsTab');"
-                    class="mb-2 d-flex align-items-center">
+                >
                     <i class="ri-file-list-3-fill ri-2x"></i>
                     <span class="nav-text ml-2">Projects</span>
                 </a>
             </li>
             <li class="nav-item main-Nav mb-2">
-                <a href="#" class="mb-2 d-flex align-items-center querterlyReportTab">
+                <a
+                    class="mb-2 d-flex align-items-center querterlyReportTab"
+                    href="#"
+                >
                     <i class="ri-draft-fill ri-2x"></i>
                     <span class="nav-text ml-2">Quarterly Report</span>
-                    <span class="ms-auto" id="dropDown_arrow">
+                    <span
+                        class="ms-auto"
+                        id="dropDown_arrow"
+                    >
                         <i class="ri-arrow-right-s-line  ri-lg d-block"></i>
                         <i class="ri-arrow-down-s-line ri-lg d-none"></i>
                     </span>
                 </a>
                 <div class="d-none sidebarTasks">
-                    <ul class="list-unstyled ps-5" id="quarterlyReportContainerLargeScreen">
+                    <ul
+                        class="list-unstyled ps-5"
+                        id="quarterlyReportContainerLargeScreen"
+                    >
 
                     </ul>
                 </div>
             </li>
         </ul>
     </nav>
-    <div class="offcanvas offcanvas-start MobileSideBar" data-bs-scroll="true" tabindex="-1" id="MobileNavOffcanvas"
-        aria-labelledby="Enable both scrolling & backdrop">
+    <div
+        class="offcanvas offcanvas-start MobileSideBar"
+        id="MobileNavOffcanvas"
+        data-bs-scroll="true"
+        aria-labelledby="Enable both scrolling & backdrop"
+        tabindex="-1"
+    >
         <div class="offcanvas-header p-0">
             <div class="nav-item mb-2 minimize w-75">
                 <div class="navlogo d-flex justify-content-center align-items-center">
-                    <img src="{{ asset('DOST_ICON.svg') }}" alt="DOST logo">
+                    <img
+                        src="{{ asset('DOST_ICON.svg') }}"
+                        alt="DOST logo"
+                    >
                     <div class="logoTitle row position-relative h-100 w-75">
                         <div class="position-absolute top-50">
                             <p class="sideTextMain text-white m-0 w-100"></p>
@@ -74,38 +103,68 @@
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn-close me-3 btn-close-white" data-bs-dismiss="offcanvas"
-                aria-label="Close"></button>
+            <button
+                class="btn-close me-3 btn-close-white"
+                data-bs-dismiss="offcanvas"
+                type="button"
+                aria-label="Close"
+            ></button>
         </div>
         <div class="offcanvas-body">
             <ul class="navbar-nav">
                 <li class="nav-item main-Nav mb-2">
-                    <a href="#" id="InformationTab"
+                    <a
+                        class="mb-2 d-flex align-items-center"
+                        id="InformationTab"
+                        href="#"
                         onclick="loadPage('{{ route('Cooperator.dashboard') }}','InformationTab');"
-                        class="mb-2 d-flex align-items-center">
+                    >
                         <i class="ri-dashboard-3-fill ri-2x"></i>
                         <span class="nav-text ml-2">Dashboard</span>
                     </a>
                 </li>
                 <li class="nav-item main-Nav mb-2">
-                    <a href="#" id="requirementTab"
+                    <a
+                        class="mb-2 d-flex align-items-center"
+                        id="requirementTab"
+                        href="#"
                         onclick="loadPage('{{ route('Cooperator.Requirements') }}','requirementTab');"
-                        class="mb-2 d-flex align-items-center">
+                    >
                         <i class="ri-file-list-2-fill ri-2x"></i>
                         <span class="nav-text ml-2">Requirements</span>
                     </a>
                 </li>
+                <li class="nav-item main-Nav mb-2">
+                    <a
+                        class="mb-2 d-flex align-items-center"
+                        id="ProjectsTab"
+                        href="#"
+                        onclick="loadPage('{{ route('Cooperator.myProjects') }}','ProjectsTab');"
+                    >
+                        <i class="ri-file-list-3-fill ri-2x"></i>
+                        <span class="nav-text ml-2">Projects</span>
+                    </a>
+                </li>
                 <li class="nav-item mb-2">
-                    <a href="#" class="mb-2 d-flex align-items-center querterlyReportTab">
+                    <a
+                        class="mb-2 d-flex align-items-center querterlyReportTab"
+                        href="#"
+                    >
                         <i class="ri-draft-fill ri-2x"></i>
                         <span class="nav-text ml-2">Quarterly Report</span>
-                        <span class="ms-auto" id="dropDown_arrow">
+                        <span
+                            class="ms-auto"
+                            id="dropDown_arrow"
+                        >
                             <i class="ri-arrow-right-s-line  ri-lg d-block"></i>
                             <i class="ri-arrow-down-s-line ri-lg d-none"></i>
                         </span>
                     </a>
                     <div class="d-none sidebarTasks">
-                        <ul class="list-unstyled ps-5" id="quarterlyReportContainerMobileScreen">
+                        <ul
+                            class="list-unstyled ps-5"
+                            id="quarterlyReportContainerMobileScreen"
+                        >
 
                         </ul>
                     </div>
@@ -113,22 +172,40 @@
             </ul>
         </div>
     </div>
-    <div id="toggle-left-margin" class="content-row navExpanded">
+    <div
+        class="content-row navExpanded"
+        id="toggle-left-margin"
+    >
         <div class="topNav shadow-sm px-3 container-fluid">
             <div class="d-flex align-items-center justify-content-between">
-                <button type="button" class="btn sideNavButtonLargeScreen">
+                <button
+                    class="btn sideNavButtonLargeScreen"
+                    type="button"
+                >
                     <i class="ri-menu-unfold-fill ri-2x"></i>
                 </button>
-                <button type="button" class="btn sideNavButtonSmallScreen">
+                <button
+                    class="btn sideNavButtonSmallScreen"
+                    type="button"
+                >
                     <i class="ri-menu-unfold-fill ri-2x"></i>
                 </button>
             </div>
             <ul class="list-unstyled d-flex align-items-center m-0 gap-3 ">
                 <li class="nofi-li">
-                    <a class="position-relative text-decoration-none nav-link" data-bs-toggle="dropdown"
-                        href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                    <a
+                        class="position-relative text-decoration-none nav-link"
+                        data-bs-toggle="dropdown"
+                        href="#"
+                        role="button"
+                        aria-haspopup="false"
+                        aria-expanded="false"
+                    >
                         <i class="ri-notification-3-line ri-2x"></i>
-                        <div id="badge--container" style="display: none;"></div>
+                        <div
+                            id="badge--container"
+                            style="display: none;"
+                        ></div>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg py-0">
                         <div class="p-2 border-top-0 border-start-0 border-end-0 border-dashed border">
@@ -137,24 +214,39 @@
                                     <p class="m-0 font-16 fw-semibold">Notification</p>
                                 </div>
                                 <div class="col-auto">
-                                    <a href="#" class="text-dark text-decoration-underline">
+                                    <a
+                                        class="text-dark text-decoration-underline"
+                                        href="#"
+                                    >
                                         <small>Clear All</small>
                                     </a>
                                 </div>
                             </div>
                         </div>
-                        <div class="px-2" id="notification--container"
-                            style="max-height: 300px; overflow-y: auto;">
+                        <div
+                            class="px-2"
+                            id="notification--container"
+                            style="max-height: 300px; overflow-y: auto;"
+                        >
 
                         </div>
-                        <a href="#" class="dropdown-item text-center text-primary notify-item border-top py-2">
+                        <a
+                            class="dropdown-item text-center text-primary notify-item border-top py-2"
+                            href="#"
+                        >
                             View All
                         </a>
                     </div>
                 </li>
                 <li class="avatar-li">
-                    <a class="text-decoration-none nav-link" data-bs-toggle="dropdown" href="#" role="button"
-                        aria-haspopup="false" aria-expanded="false">
+                    <a
+                        class="text-decoration-none nav-link"
+                        data-bs-toggle="dropdown"
+                        href="#"
+                        role="button"
+                        aria-haspopup="false"
+                        aria-expanded="false"
+                    >
                         <div class="account-avatar d-flex align-items-center justify-content-center gap-2">
                             <div class="profile-logo rounded-circle border border-1 border-white bg-primary">
                                 {{ strtoupper(substr(trim((string) Auth::user()->coopUserInfo->f_name), 0, 1)) }}
@@ -164,8 +256,10 @@
                             </p>
                         </div>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated py-0"
-                        style="max-height: 300px; width:10vw;">
+                    <div
+                        class="dropdown-menu dropdown-menu-end dropdown-menu-animated py-0"
+                        style="max-height: 300px; width:10vw;"
+                    >
                         <div class="p-2 border-top-0 border-start-0 border-end-0 border-dashed border">
                             <div class="row align-items-center">
                                 <div class="col">
@@ -174,12 +268,20 @@
                             </div>
                         </div>
                         <div class="px-2">
-                            <button type="button" class="dropdown-item py-2" data-bs-toggle="modal"
-                                data-bs-target="#myAccountModal">
+                            <button
+                                class="dropdown-item py-2"
+                                data-bs-toggle="modal"
+                                data-bs-target="#myAccountModal"
+                                type="button"
+                            >
                                 <p><i class="ri-user-3-line me-2"></i>My Account</p>
                             </button>
-                            <a href="#" class="dropdown-item py-2"
-                                data-bs-toggle="modal" data-bs-target="#logoutConfirmationModal">
+                            <a
+                                class="dropdown-item py-2"
+                                data-bs-toggle="modal"
+                                data-bs-target="#logoutConfirmationModal"
+                                href="#"
+                            >
                                 <p><i class="ri-logout-box-line me-2"></i>Logout</p>
                             </a>
                         </div>
@@ -189,11 +291,18 @@
         </div>
         <div class="main-content">
             <div class="spinner d-flex justify-content-center align-items-center h-100 d-none">
-                <div class="spinner-border" style="width: 50px; height: 50px;" role="status">
+                <div
+                    class="spinner-border"
+                    role="status"
+                    style="width: 50px; height: 50px;"
+                >
                     <span class="visually-hidden">Loading...</span>
                 </div>
             </div>
-            <main class="main-column scrollable-main" id="main-content">
+            <main
+                class="main-column scrollable-main"
+                id="main-content"
+            >
             </main>
         </div>
     </div>
@@ -231,3 +340,4 @@
     }
 </script>
 @vite('resources/js/coopPage.js')
+
