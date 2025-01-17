640ff8979a19ccdef645c01ce0ac19654d99f1a7
Reanz_Arthur <reqbahrf@gmail.com>
2025-01-17 08:00:27 +0800

    feat: Create ApplicationForm model

---
+++ app/Models/ApplicationForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ApplicationForm extends Model
+{
+    protected $table = 'application_forms';
+
+    protected $fillable = [
+        'business_id',
+        'key',
+        'data',
+        'status',
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    public function businessInfo(): BelongsTo
+    {
+        return $this->belongsTo(BusinessInfo::class, 'business_id', 'id');
+    }
+}
