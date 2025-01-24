caef8af89b9a441931b1fefe28c3e39326608457
Reanz_Arthur <reqbahrf@gmail.com>
2025-01-24 20:37:05 +0800

    refactor: vite configuration update

--- vite.config.js
+++ vite.config.js
@@ -7,22 +7,30 @@ export default defineConfig({
             host: 'localhost',
         },
         watch: {
-            usePolling: true
-        }
+            usePolling: false,
+            ignored: [
+                '**/node_modules/**',
+                '**/resources/views/components/**',
+                '**/resources/views/StaffView/outputs/**',
+                '**/*.log',
+                '**/.git/**',
+            ],
+        },
     },
     plugins: [
         laravel({
             input: [
-                    'resources/css/indexPage.css',
-                    'resources/css/app.scss',
-                    'resources/css/staffPage.css',
-                    'resources/css/adminPage.css',
-                    'resources/css/coopPage.css',
-                    'resources/js/app.js',
-                    'resources/js/applicationPage.js',
-                    'resources/js/coopPage.js',
-                    'resources/js/staffPage.js',
-                    'resources/js/adminPage.js',],
+                'resources/css/indexPage.css',
+                'resources/css/app.scss',
+                'resources/css/staffPage.css',
+                'resources/css/adminPage.css',
+                'resources/css/coopPage.css',
+                'resources/js/app.js',
+                'resources/js/applicationPage.js',
+                'resources/js/coopPage.js',
+                'resources/js/staffPage.js',
+                'resources/js/adminPage.js',
+            ],
             refresh: true,
         }),
     ],
