f7522a6d04f6d2ab12b44044415aea1c898cd6fe
Reanz_Arthur <reqbahrf@gmail.com>
2025-02-04 12:32:37 +0800

    feat: add new foreign key 'application_id' on the add tp the `application_forms`

---
+++ database/migrations/2025_02_04_122603_add_application_id_column_to_application_forms.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('application_forms', function (Blueprint $table) {
+            $table->bigInteger('application_id')->unsigned()->nullable()->after('business_id');
+            $table->foreign('application_id')->references('id')->on('application_info')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('application_forms', function (Blueprint $table) {
+            $table->dropForeign(['application_id']);
+            $table->dropColumn('application_id');
+        });
+    }
+};
