5cdf197c9cf9d529aa151ded25641d8bfaf5551d
Reanz_Arthur <reqbahrf@gmail.com>
2025-02-24 16:24:09 +0800

    feat: Implement due payment notification command
    This commit introduces a new console command, `app:check-due-payments`, that checks for due payments and sends email notifications to users. The command identifies due payments based on the `payment_status` (Due, Overdue, Pending) and a due date 15 days in the future. It then retrieves the associated user for each project and sends a `DuePayment` notification containing the due date, project ID, and amount. Logging and error handling have been implemented to ensure the process is monitored and any exceptions are caught and logged.

---
+++ app/Console/Commands/CheckDuePayments.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use App\Models\User;
+use App\Models\ProjectInfo;
+use App\Models\PaymentRecord;
+use Illuminate\Support\Carbon;
+use Illuminate\Console\Command;
+use App\Notifications\DuePayment;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+
+
+class CheckDuePayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:check-due-payments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check due payments and send email and notifications';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Checking due payments...');
+        try {
+            $projectIDs = $this->getUniqueProjectIds();
+            foreach ($projectIDs as $projectId) {
+                $duePayments = $this->getDuePayments($projectId);
+                if ($duePayments->isNotEmpty()) {
+                    $this->info("Found " . $duePayments->count() . " due payments for project ID: {$projectId}" . " that will be due on: " . Carbon::now()->addDays(15)->format('Y-m-d'));
+                    $this->processDuePayments($duePayments);
+                } else {
+                    $this->info("No due payments found for project ID: {$projectId} with due date: " . Carbon::now()->addDays(15)->format('Y-m-d'));
+                }
+            }
+            return Command::SUCCESS;
+        } catch (Exception $e) {
+            Log::error('Error checking due payments: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+    }
+    protected function getUniqueProjectIds(): Collection
+    {
+        try {
+            $projectID = PaymentRecord::distinct()->pluck('Project_id');
+            return $projectID;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected function getDuePayments(string $projectId): Collection
+    {
+        try {
+
+            $this->info("Getting due payments for project ID: {$projectId}");
+            $dueDate = now()->addDays(15);
+            $duePayments = PaymentRecord::wherein('payment_status', ['Due', 'Overdue', 'Pending'])
+                ->whereDate('due_date', $dueDate)
+                ->where('Project_id', $projectId)
+                ->get();
+
+            return $duePayments;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected function processDuePayments(Collection $duePayments): void
+    {
+        try {
+            $this->info('Processing due payments...');
+
+            foreach ($duePayments as $duePayment) {
+                $this->sendEmailAndNotification($duePayment->due_date, $duePayment->Project_id, $duePayment->amount);
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    protected function sendEmailAndNotification(string $dueDate, string $projectId, string $dueAmount): void
+    {
+        try {
+            $this->info('Sending email and notification for Project ID: ' . $projectId);
+
+            // Retrieve the user associated with the project
+            $notifiableUser = ProjectInfo::with('businessInfo.userInfo.user')->where('Project_id', $projectId)->first();
+
+            if (!$notifiableUser || !$notifiableUser->businessInfo || !$notifiableUser->businessInfo->userInfo || !$notifiableUser->businessInfo->userInfo->user) {
+                $this->info('No notifiable user found for Project ID: ' . $projectId);
+                return;
+            }
+
+            // Assuming 'user' is the relationship that contains the User model
+            $user = $notifiableUser->businessInfo->userInfo->user;
+            $user->notify(new DuePayment($dueDate, $projectId, $dueAmount));
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
