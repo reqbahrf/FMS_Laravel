082319e5cd2ec2d75ef3223d5115c80010e61d6a
Reanz_Arthur <reqbahrf@gmail.com>
2025-02-25 08:32:21 +0800

    refactor: :fire: remove the default artisan command in the `console.php`

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('reports:close')->daily();
 Schedule::command('app:check-due-payments')->dailyAt('09:00');
