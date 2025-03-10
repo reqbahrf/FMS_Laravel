7341bbf897d650687be0e38369e59d03e1b93f9d
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-10 11:34:03 +0800

    feat(file service): add file service class.

---
+++ app/Services/FileService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class FileService
+{
+    public function storeToBusinessDirectory() {}
+
+    public function getFromBusinessDirectory() {}
+
+    public function storeToTempDirectory() {}
+
+    public function getFromTempDirectory() {}
+
+    public function deleteFromBusinessDirectory() {}
+
+    public function deleteFromTempDirectory() {}
+}
