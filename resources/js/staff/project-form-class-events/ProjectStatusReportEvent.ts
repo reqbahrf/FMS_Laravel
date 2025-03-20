6c0fdd0feb33418d233c41f33575239bbb354ab8
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-20 12:21:52 +0800

    Add numeric input formatting for project status report fields

--- resources/js/staff/project-form-class-events/ProjectStatusReportEvent.ts
+++ resources/js/staff/project-form-class-events/ProjectStatusReportEvent.ts
@@ -3,6 +3,7 @@ import {
     addNewRowHandler,
     removeRowHandler,
 } from '../../Utilities/add-and-remove-table-row-handler';
+import { customFormatNumericInput } from '../../Utilities/input-utils';
 export default class ProjectStatusReportEvent {
     private form: JQuery<HTMLFormElement>;
     private parentFormWrapper: JQuery<HTMLElement>;
@@ -12,12 +13,66 @@ export default class ProjectStatusReportEvent {
         this.parentFormWrapper = this.form.closest('#formWrapper');
         this.floatingWindowContainer =
             this.parentFormWrapper.find('#floating-window');
+        this._initializeInputFormatters();
         this._initializeFloatingWindow();
         this._initializeAddAndRemoveTableRow();
     }
 
+    private _initializeInputFormatters() {
+        customFormatNumericInput(
+            this.form.find('div#projectInfo table tbody td'),
+            'input.amount_of_setup_assistance'
+        );
+        customFormatNumericInput(
+            this.form.find('table#equipmentAndFacilitiesTable tbody'),
+            [
+                'td input.approved_qty',
+                'td input.approved_cost',
+                'td input.actual_qty',
+                'td input.actual_cost',
+            ]
+        );
+        customFormatNumericInput(
+            this.form.find('table#nonEquipmentItemsTable tbody'),
+            [
+                'td input.approved_qty',
+                'td input.approved_cost',
+                'td input.actual_qty',
+                'td input.actual_cost',
+            ]
+        );
+
+        customFormatNumericInput(
+            this.form.find('div#statusFundUtilization table'),
+            [
+                'td input[name="total_approved_project_cost"]',
+                'td input[name="amount_utilized_per_financial_report"]',
+            ]
+        );
+
+        customFormatNumericInput(this.form.find('div#statusOfRefund table'), [
+            'td input[name="total_amount_to_be_refunded"]',
+            'td input[name="total_amount_already_due"]',
+            'td input[name="total_amount_refunded"]',
+        ]);
+
+        customFormatNumericInput(
+            this.form.find('table#volumeAndValueProductionTable tbody'),
+            'td input.sales_gross_sales'
+        );
+
+        customFormatNumericInput(
+            this.form.find('table#indirectEmploymentTable tbody'),
+            [
+                'td input.forward_male',
+                'td input.forward_female',
+                'td input.backward_male',
+                'td input.backward_female',
+            ]
+        );
+    }
+
     private _initializeAddAndRemoveTableRow() {
-        console.log('Initializing add and remove table row');
         try {
             addNewRowHandler(
                 '#addEquipmentAndFacilitiesRow',
