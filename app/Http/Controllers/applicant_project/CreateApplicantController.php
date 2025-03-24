35b25035fc4db3e1011b278cf129a16facfbf262
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-24 13:27:47 +0800

    Add AJAX check to CreateApplicantController show method

--- app/Http/Controllers/applicant_project/CreateApplicantController.php
+++ app/Http/Controllers/applicant_project/CreateApplicantController.php
@@ -36,10 +36,13 @@ class CreateApplicantController extends Controller
         return view('staff-view.staff-index');
     }
 
-    public function show($ownerId)
+    public function show(Request $request, $ownerId)
     {
-        $draft_type = $this->registrationService->getDraftType($ownerId);
-        return view('components.applicant-form', compact('ownerId', 'draft_type'));
+        if ($request->ajax()) {
+            $draft_type = $this->registrationService->getDraftType($ownerId);
+            return view('components.applicant-form', compact('ownerId', 'draft_type'));
+        }
+        return view('staff-view.staff-index');
     }
     public function storeApplicantDetail(ApplicantInfoRequest $request): JsonResponse
     {
