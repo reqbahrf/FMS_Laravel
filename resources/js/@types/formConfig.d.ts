7f5fd54dfec4449fded4bfdd6ba7a5708697189f
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-25 14:02:38 +0800

    refactor: move formConfig.d.ts into a @type folder.

---
+++ resources/js/@types/formConfig.d.ts
@@ -0,0 +1,26 @@
+declare module 'global-form-config' {
+    
+    export interface TableRowConfig {
+        createRow: (rowData: any) => string;
+    }
+    
+    export interface TableSelectors {
+        [key: string]: string;
+    }
+    
+    export interface TableRowConfigs {
+        [key: string]: TableRowConfig;
+    }
+    
+    export interface DraftFormConfig {
+        formSelector: string;
+        tableSelectors: TableSelectors;
+        tableRowConfigs: TableRowConfigs;
+        filepondSelector: string[];
+    }
+
+    global {
+        var FormConfig: DraftFormConfig;
+    }
+
+}
