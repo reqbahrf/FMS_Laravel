1d12946f346695a595e055fb69d04daab8d0179a
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-26 08:52:09 +0800

    Add breadcrumb navigation and header to detailed applicant info form

--- resources/views/components/detailed-applicant-info-form.blade.php
+++ resources/views/components/detailed-applicant-info-form.blade.php
@@ -1,7 +1,20 @@
+@props(['ownerId', 'draft_type', 'withAction' => false])
+<div>
+    <h4 class="p-3">Fill Applicant Information</h4>
+</div>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb px-3">
+        <li class="breadcrumb-item"><a
+                href="#"
+                onclick="loadPage('{{ route('staff.Project') }}', 'projectLink')"
+            >Projects</a></li>
+        <li class="breadcrumb-item active">Fill Applicant Information</li>
+    </ol>
+</nav>
 <x-application-form.form
-    :ownerId="$ownerId"
-    :draft_type="$draft_type"
-    :withAction="false"
+    :$ownerId
+    :$draft_type
+    :$withAction
 >
     <x-application-form.personal-info />
     <x-application-form.business-info :withFileInput="false" />
