e3048f1eb98d21983b1485b9d25f554c96ed7736
Reanz_Arthur <reqbahrf@gmail.com>
2025-03-28 09:06:19 +0800

    Add !important to font-size styles in document header

--- resources/views/components/document-header.blade.php
+++ resources/views/components/document-header.blade.php
@@ -14,9 +14,10 @@
             </td>
             <td style="text-align: left; vertical-align: top;">
 
-                <h1 style="font-size: 13px; margin: 0; font-weight: normal;">Republic of the Philippines</h1>
-                <h2 style="font-size: 16px; margin: 0; font-weight: bold;">DEPARTMENT OF SCIENCE AND TECHNOLOGY</h2>
-                <h3 style="font-size: 13px; margin: 0; font-weight: semi-bold;">REGIONAL OFFICE NO. XI</h3>
+                <h1 style="font-size: 13px !important; margin: 0; font-weight: normal;">Republic of the Philippines</h1>
+                <h2 style="font-size: 16px !important; margin: 0; font-weight: bold;">DEPARTMENT OF SCIENCE AND
+                    TECHNOLOGY</h2>
+                <h3 style="font-size: 13px !important; margin: 0; font-weight: semi-bold;">REGIONAL OFFICE NO. XI</h3>
             </td>
             <td style="width: 300px; vertical-align: top; text-align: right;">
                 <img
